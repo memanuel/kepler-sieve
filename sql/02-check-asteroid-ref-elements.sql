6aaf8d53581d4d881da5a775bb08ac602c8f447b
Michael S. Emanuel <michael.s.emanuel@gmail.com>
2021-02-14 21:45:45 -0500

    example call to mysqlsh util.import_table

--- sql/02-check-asteroid-ref-elements.sql
+++ sql/02-check-asteroid-ref-elements.sql
@@ -1,46 +1,37 @@
+-- Count number of asteroids that have references pushed forward 
 SELECT
-	elt_jpl.AsteroidNumber,
-	elt_jpl.AsteroidName,
-	elt_jpl.a AS a_jpl,
-	elt_ks.a AS a_ks,	
-	elt_ks.a - elt_jpl.a AS a_err,
-	elt_ks.e - elt_jpl.e AS e_err,
-	elt_ks.inc - elt_jpl.inc AS inc_err,
-	elt_ks.Omega_node - elt_jpl.Omega_node AS Omega_err,
-	elt_ks.omega_peri - elt_jpl.omega_peri AS omega_err,
-	elt_ks.M - elt_jpl.M AS M_err,
-	elt_ks.f - elt_jpl.f AS f_err
-FROM
-	JPL.AsteroidElement AS elt_jpl
-	LEFT JOIN KS.Asteroid AS ast ON ast.AsteroidNumber = elt_jpl.asteroidNumber
-	INNER JOIN KS.DailyTime AS dt ON dt.MJD = elt_jpl.epoch
-	LEFT JOIN KS.AsteroidElement_Ref AS elt_ks ON
-		elt_ks.AsteroidID = ast.AsteroidID AND
-		elt_ks.TimeID = dt.TimeID
-ORDER BY elt_jpl.AsteroidNumber
-LIMIT 100;
-
-WITH t1 AS(
-SELECT
-	elt_jpl.AsteroidNumber,
-	elt_jpl.AsteroidName,
-	elt_jpl.a AS a_jpl,
-	elt_ks.a AS a_ks,	
-	elt_ks.a - elt_jpl.a AS a_err,
-	elt_ks.e - elt_jpl.e AS e_err,
-	elt_ks.inc - elt_jpl.inc AS inc_err,
-	elt_ks.Omega_node - elt_jpl.Omega_node AS Omega_node_err,
-	elt_ks.omega_peri - elt_jpl.omega_peri AS omega_peri_err,
-	elt_ks.M - elt_jpl.M AS M_err,
-	elt_ks.f - elt_jpl.f AS f_err
+	COUNT(elt_ks.AsteroidID) AS ast_count
 FROM
 	JPL.AsteroidElement AS elt_jpl
 	INNER JOIN KS.Asteroid AS ast ON ast.AsteroidNumber = elt_jpl.asteroidNumber
-	INNER JOIN KS.DailyTime AS dt ON dt.MJD = elt_jpl.epoch
+	INNER JOIN KS.DailyTime AS dt ON dt.MJD = 59000
 	INNER JOIN KS.AsteroidElement_Ref AS elt_ks ON
 		elt_ks.AsteroidID = ast.AsteroidID AND
 		elt_ks.TimeID = dt.TimeID
-)
+WHERE elt_jpl.epoch < elt_ks.epoch;
+
+
+-- Compare the elements to see if they are close
 SELECT
-	sqrt(AVG(t1.a_err*t1.a_err)) AS a_err
-FROM t1;
+	elt_jpl.AsteroidNumber,
+	elt_jpl.AsteroidName,
+	elt_jpl.epoch,
+	elt_jpl.a AS a_jpl,
+	elt_ks.a AS a_ks,	
+	elt_ks.a - elt_jpl.a AS da,
+	elt_ks.e - elt_jpl.e AS de,
+	elt_ks.inc - elt_jpl.inc AS dinc,
+	MOD(elt_ks.Omega_node - elt_jpl.Omega_node + 2.0*PI(), 2.0*PI()) AS dOmega,
+	MOD(elt_ks.omega_peri - elt_jpl.omega_peri + 2.0*PI(), 2.0*PI()) AS domega,
+	MOD(elt_ks.M - elt_jpl.M + 2.0*PI(), 2.0*PI()) AS dM
+FROM
+	JPL.AsteroidElement AS elt_jpl
+	INNER JOIN KS.Asteroid AS ast ON ast.AsteroidNumber = elt_jpl.asteroidNumber
+	INNER JOIN KS.DailyTime AS dt ON dt.MJD = 59000
+	INNER JOIN KS.AsteroidElement_Ref AS elt_ks ON
+		elt_ks.AsteroidID = ast.AsteroidID AND
+		elt_ks.TimeID = dt.TimeID
+WHERE elt_jpl.epoch < elt_ks.epoch		
+ORDER BY elt_jpl.AsteroidNumber
+LIMIT 100;
+
