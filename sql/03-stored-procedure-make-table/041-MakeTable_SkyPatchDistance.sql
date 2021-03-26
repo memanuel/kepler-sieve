6e098df9cdb47a848171c0dd44f619debecbce4a
Michael S. Emanuel <michael.s.emanuel@gmail.com>
2021-03-26 13:38:11 -0400

    new workflow for  SkyPatchGrid, SkyPatchGridDistance and SkyPatch working.  SkyPatchDistance in progress

--- sql/03-stored-procedure-make-table/041-MakeTable_SkyPatchDistance.sql
+++ sql/03-stored-procedure-make-table/041-MakeTable_SkyPatchDistance.sql
@@ -96,8 +96,18 @@ SELECT
 FROM
 	t2;
 
+-- Set the bounds on u, v, w
+-- UPDATE KS.SkyPatchGrid
+-- SET
+-- 	uMin = LEAST(u00, u01, u10, u11),
+-- 	uMax = GREATEST(u00, u01, u10, u11),
+-- 	vMin = LEAST(v00, v01, v10, v11),
+-- 	vMax = GREATEST(v00, v01, v10, v11),
+-- 	wMin = LEAST(w00, w01, w10, w11),
+-- 	wMax = GREATEST(w00, w01, w10, w11);
 -- Populate SkyPatchGridDistance by querying over 16 possible pairs of points
 -- Compare 4 corners on p1 with 4 corners on p2 and take the minimum
+
 INSERT INTO KS.SkyPatchGridDistance
 (i1, j1, i2, j2, dr_mid, dr_min)
 WITH t1 AS (
@@ -158,8 +168,63 @@ FROM
 WHERE
 	dr_min < dr_max;
 
+
+
 END $$
 
+-- *********************************************************************************
+CREATE OR REPLACE 
+DEFINER = kepler
+PROCEDURE KS.MakeTable_SkyPatch()
+COMMENT "Populate the KS.SkyPatchGrid and KS.SkyPatchGridDistance tables"
+BEGIN 
+
+-- Calculate @M and @N
+SELECT 
+	MAX(spg.i)+1 AS M 
+INTO @M 
+FROM KS.SkyPatchGrid AS spg;
+SET @N = (@M DIV 2);	
+
+TRUNCATE TABLE KS.SkyPatch;
+
+-- Build SkyPatch by joining CubeFace to SkyPatchGrid
+INSERT INTO KS.SkyPatch
+(SkyPatchID, CubeFaceID, i, j, x, y, z, x00, y00, z00, x01, y01, z01, x10, y10, z10, x11, y11, z11)
+SELECT
+	-- Integer IDs
+	(cf.CubeFaceID-1)*@M*@M + 2*gr.i*@N + j AS SkyPatchID,
+	cf.CubeFaceID,
+	-- Local grid coordinates (i, j) on the major face
+	gr.i,
+	gr.j,
+	-- Coordinates of midpoint (x, y, z)
+	IF(cf.alpha='X', gr.u, 0.0) + IF(cf.beta='X', gr.v, 0.0) + IF(cf.gamma='X', gr.w*cf.ci, 0.0) AS x,
+	IF(cf.alpha='Y', gr.u, 0.0) + IF(cf.beta='Y', gr.v, 0.0) + IF(cf.gamma='Y', gr.w*cf.ci, 0.0) AS y,
+	IF(cf.alpha='Z', gr.u, 0.0) + IF(cf.beta='Z', gr.v, 0.0) + IF(cf.gamma='Z', gr.w*cf.ci, 0.0) AS z,
+	-- Coordinates of lower left corner (x00, y00, z00)
+	IF(cf.alpha='X', gr.u00, 0.0) + IF(cf.beta='X', gr.v00, 0.0) + IF(cf.gamma='X', gr.w00*cf.ci, 0.0) AS x00,
+	IF(cf.alpha='Y', gr.u00, 0.0) + IF(cf.beta='Y', gr.v00, 0.0) + IF(cf.gamma='Y', gr.w00*cf.ci, 0.0) AS y00,
+	IF(cf.alpha='Z', gr.u00, 0.0) + IF(cf.beta='Z', gr.v00, 0.0) + IF(cf.gamma='Z', gr.w00*cf.ci, 0.0) AS z00,
+	-- Coordinates of upper left corner (x01, y01, z01)
+	IF(cf.alpha='X', gr.u01, 0.0) + IF(cf.beta='X', gr.v01, 0.0) + IF(cf.gamma='X', gr.w01*cf.ci, 0.0) AS x01,
+	IF(cf.alpha='Y', gr.u01, 0.0) + IF(cf.beta='Y', gr.v01, 0.0) + IF(cf.gamma='Y', gr.w01*cf.ci, 0.0) AS y01,
+	IF(cf.alpha='Z', gr.u01, 0.0) + IF(cf.beta='Z', gr.v01, 0.0) + IF(cf.gamma='Z', gr.w01*cf.ci, 0.0) AS z01,
+	-- Coordinates of lower right corner (x10, y10, z10)
+	IF(cf.alpha='X', gr.u10, 0.0) + IF(cf.beta='X', gr.v10, 0.0) + IF(cf.gamma='X', gr.w10*cf.ci, 0.0) AS x10,
+	IF(cf.alpha='Y', gr.u10, 0.0) + IF(cf.beta='Y', gr.v10, 0.0) + IF(cf.gamma='Y', gr.w10*cf.ci, 0.0) AS y10,
+	IF(cf.alpha='Z', gr.u10, 0.0) + IF(cf.beta='Z', gr.v10, 0.0) + IF(cf.gamma='Z', gr.w10*cf.ci, 0.0) AS z10,
+	-- Coordinates of upper right corner (x11, y11, z11)
+	IF(cf.alpha='X', gr.u11, 0.0) + IF(cf.beta='X', gr.v11, 0.0) + IF(cf.gamma='X', gr.w11*cf.ci, 0.0) AS x11,
+	IF(cf.alpha='Y', gr.u11, 0.0) + IF(cf.beta='Y', gr.v11, 0.0) + IF(cf.gamma='Y', gr.w11*cf.ci, 0.0) AS y11,
+	IF(cf.alpha='Z', gr.u11, 0.0) + IF(cf.beta='Z', gr.v11, 0.0) + IF(cf.gamma='Z', gr.w11*cf.ci, 0.0) AS z11
+FROM
+	KS.CubeFace AS cf CROSS JOIN
+	KS.SkyPatchGrid AS gr
+ORDER BY SkyPatchID;
+
+END $$
+	
 -- *********************************************************************************
 CREATE OR REPLACE 
 DEFINER = kepler
@@ -203,7 +268,14 @@ BEGIN
 
 -- Empty table first so this procedure can be re-run on demand
 TRUNCATE TABLE KS.SkyPatchDistance;
-	
+
+-- Calculate @M and @N
+SELECT 
+	MAX(spg.i)+1 AS M 
+INTO @M 
+FROM KS.SkyPatchGrid AS spg;
+SET @N = (@M DIV 2);
+
 -- Most of the neighbors are on the same major cube face
 INSERT INTO KS.SkyPatchDistance
 (SkyPatchID_1, SkyPatchID_2, dr_mid, dr_min)
@@ -281,13 +353,13 @@ FROM
 	-- Wrap to the relevant grid cell
 	INNER JOIN KS.SkyPatch AS p2 ON
 		p2.CubeFaceID = cf2.CubeFaceID AND
- 		p2.i IN (p1.i, p1.j, 0, 2*@N-1) AND
- 		p2.j IN (p1.i, p1.j, 0, 2*@N-1)			
+ 		p2.i IN (p1.i, p1.j, 0, @M-1) AND
+ 		p2.j IN (p1.i, p1.j, 0, @M-1)			
 WHERE
 	-- The coordinate index for the perimeter is 1 (u) or 2 (v)
 	(pk._ BETWEEN 1 AND 2) AND
 	-- Two possible settings for i or j to lie on the perimeter of a face
-	(pv._ = 0 OR pv._ = 2*@N-1)
+	(pv._ = 0 OR pv._ = @M-1)
 )
 SELECT
 	t1.SkyPatchID_1,
@@ -297,17 +369,20 @@ SELECT
 FROM
 	t1
 WHERE
-	t1.dr_mid < 1.0 / @N;
+	t1.dr_mid < (1.0 / @N);
 
 -- Create a third batch by searching along both grid faces; save in a temp table
 CREATE OR REPLACE TEMPORARY TABLE KS.SkyPatchDistance_batch LIKE KS.SkyPatchDistance;
 -- Need to drop PK because this query will generate the same candidate multiple times
 ALTER TABLE KS.SkyPatchDistance_batch DROP PRIMARY KEY;
+ALTER TABLE KS.SkyPatchDistance_batch DROP dr_mid;
 ALTER TABLE KS.SkyPatchDistance_batch DROP dr_min;
+-- Upper bound on dr_mid
+ALTER TABLE KS.SkyPatchDistance_batch ADD COLUMN dr_mid_ub DOUBLE NOT NULL;
 
 -- Generate candidate paths from one SkyPatch to another within the maximum distance
 INSERT INTO KS.SkyPatchDistance_batch
-(SkyPatchID_1, SkyPatchID_2, dr_mid)
+(SkyPatchID_1, SkyPatchID_2, dr_mid_ub)
 WITH t1 AS (
 SELECT
 	p1.SkyPatchID AS SkyPatchID_1,
