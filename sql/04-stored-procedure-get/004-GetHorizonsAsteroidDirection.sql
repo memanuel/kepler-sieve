2ab49f23ebb975c466829a93cc9e72ab5ee07022
Michael S. Emanuel <michael.s.emanuel@gmail.com>
2021-06-11 12:59:09 -0400

    revised asteroid directions from JPL used in testing

--- sql/04-stored-procedure-get/004-GetHorizonsAsteroidDirection.sql
+++ sql/04-stored-procedure-get/004-GetHorizonsAsteroidDirection.sql
@@ -21,7 +21,11 @@ SELECT
 	adi.DEC_ast,
 	adi.RA_app,
 	adi.DEC_app,
-	adi.LightTime
+	adi.LightTime,
+	adi.r,
+	adi.rDot,
+	adi.delta,
+	adi.deltaDot
 FROM
 	JPL.AsteroidDirectionImport AS adi
 	INNER JOIN KS.IntegrationTime AS it ON it.MJD = (adi.JD - 2400000.5)
@@ -36,10 +40,11 @@ $$
 CREATE OR REPLACE 
 DEFINER = kepler
 PROCEDURE JPL.GetAsteroidDirection(
+	IN ObservatoryShortName VARCHAR(16),
     IN n0 INT,
     IN n1 INT
 )
-COMMENT "Get asteroid directions imported from Horizons."
+COMMENT "Get asteroid directions imported from Horizons. ObservatoryShortName one of 'Geocenter' and 'ZTF'"
 
 BEGIN 
 
@@ -49,7 +54,8 @@ SELECT
 	ad.AsteroidID,
 	ad.ObservatoryID,
 	ad.TimeID,
-	ad.mjd,
+	-- Departure time of light
+	ad.mjd - ad.LightTime / 1440.0 AS tAst,
 	-- Quoted astrometric RA/DEC
 	ad.RA_ast,
 	ad.DEC_ast,
@@ -64,12 +70,19 @@ SELECT
 	ad.ux_app,
 	ad.uy_app,
 	ad.uz_app,
-	-- Light time
-	ad.LightTime
+	-- Light time and observation time
+	ad.LightTime,
+	ad.mjd AS tObs
 FROM
+	-- The JPL directions
 	JPL.AsteroidDirection AS ad
+	-- The observatory
+	INNER JOIN KS.Observatory AS o ON o.ObservatoryID = ad.ObservatoryID
 WHERE
+	-- Only the selected asteroids
     ad.AsteroidID BETWEEN n0 AND (n1-1)
+    -- Only the selected observatory
+    AND o.ObservatoryShortName = ObservatoryShortName
 ORDER BY ad.AsteroidID, ad.ObservatoryID, ad.TimeID;
 
 END
@@ -79,7 +92,7 @@ $$
 CREATE OR REPLACE 
 DEFINER = kepler
 PROCEDURE JPL.AstrometricDirectionTest()
-COMMENT "Get asteroid directions and positions to test MSE calculations."
+COMMENT "Get asteroid directions and positions to test MSE calculations; vectors from JPL."
 
 BEGIN 
 
@@ -107,9 +120,14 @@ SELECT
 	-- Astrometric direction from JPL RA/DEC
 	ad.ux_ast AS ux,
 	ad.uy_ast AS uy,
-	ad.uz_ast AS uz
+	ad.uz_ast AS uz,
+	-- Distance
+	ad.delta
 FROM
+	-- The JPL asteroid directions
 	JPL.AsteroidDirection AS ad
+	-- The observatory
+	INNER JOIN KS.Observatory AS o ON o.ObservatoryID=ad.ObservatoryID
 	-- Position of Earth
 	INNER JOIN JPL.HorizonsBody AS hbe ON hbe.HorizonsBodyName='LB.Earth'
 	INNER JOIN JPL.HorizonsVectors AS hve ON 
@@ -123,6 +141,65 @@ FROM
 WHERE 
 	-- Only test the date range of MSE integrated orbits
 	ad.TimeID BETWEEN 48000*24*60 AND 63000*24*60
+	-- Only return Earth geocenter directions
+	AND o.ObservatoryShortName='Geocenter'
+ORDER BY ad.AsteroidID, ad.TimeID;
+
+END
+$$
+
+-- ********************************************************************************
+CREATE OR REPLACE 
+DEFINER = kepler
+PROCEDURE JPL.AstrometricDirectionAndVectorTest()
+COMMENT "Get asteroid directions and positions to test MSE calculations; vectors from MSE not JPL."
+
+BEGIN 
+
+SELECT
+	-- Key fields
+	ad.AsteroidID,
+	ad.TimeID,
+	-- Time light leaves the asteroid
+	ad.mjd AS tAst,
+	-- The Earth geocenter position
+	sve.qx AS qObs_x,
+	sve.qy AS qObs_y,
+	sve.qz AS qObs_z,
+	-- The asteroid position
+	av.qx AS qAst_x,
+	av.qy AS qAst_y,
+	av.qz AS qAst_z,
+	-- The asteroid velocity
+	av.vx AS vAst_x,
+	av.vy AS vAst_y,
+	av.vz AS vAst_z,
+	-- Light time
+	ad.LightTime,
+	ad.mjd AS tObs,
+	-- Astrometric direction from JPL RA/DEC
+	ad.ux_ast AS ux,
+	ad.uy_ast AS uy,
+	ad.uz_ast AS uz,
+	-- Distance
+	ad.delta
+FROM
+	-- The JPL asteroid directions
+	JPL.AsteroidDirection AS ad
+	-- The observatory
+	INNER JOIN KS.Observatory AS o ON o.ObservatoryID=ad.ObservatoryID
+	-- The MSE state vectors for Earth
+	INNER JOIN KS.StateVectors_Earth AS sve ON
+		sve.TimeID = ad.TimeID
+	-- The MSE state vectors for this asteroid
+	INNER JOIN KS.AsteroidVectors AS av ON 
+		av.AsteroidID = ad.AsteroidID AND
+		av.TimeID = ad.TimeID
+WHERE
+	-- Only test the date range of MSE integrated orbits
+	ad.TimeID BETWEEN 48000*24*60 AND 63000*24*60
+	-- Only return Earth geocenter directions
+	AND o.ObservatoryShortName='Geocenter'
 ORDER BY ad.AsteroidID, ad.TimeID;
 
 END
