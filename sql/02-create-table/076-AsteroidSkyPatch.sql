dc64c1411d16b51d83f7ee090ba095059bdea455
Michael S. Emanuel <michael.s.emanuel@gmail.com>
2021-06-15 10:27:33 -0400

    revised AsteroidSkyPatch table

--- sql/02-create-table/076-AsteroidSkyPatch.sql
+++ sql/02-create-table/076-AsteroidSkyPatch.sql
@@ -1,20 +1,26 @@
 CREATE OR REPLACE TABLE KS.AsteroidSkyPatch(
-	-- The time when the direction is calculated
-	IntegrationTimeID INT NOT NULL
-		COMMENT "The TimeID when the direction is calculated; sampled from TimeSliceID; foreign key to IntegrationTime table",
 	-- The asteroid
 	AsteroidID INT NOT NULL
 		COMMENT "The asteroid whose direction SkyPatch is being saved; foreign key to Asteroid table",
+	Segment INT NOT NULL
+		COMMENT "The segment number; table describes segments where asteroid is one one patch of the sky",
 	-- The SkyPatch of the direction
 	SkyPatchID INT NOT NULL
-		COMMENT "The SkyPatch where this asteroid could be found at the given IntegrationTime; foreign key to SkyPatch table",
+		COMMENT "The SkyPatch where this asteroid could be found during this segment; foreign key to SkyPatch table",
+	-- The time when the direction is calculated
+	TimeID_0 INT NOT NULL
+		COMMENT "The TimeID when this segment starts",
+	TimeID_1 INT NOT NULL
+		COMMENT "The TimeID when this segment ends",
 	-- Primary key
-	PRIMARY KEY (IntegrationTimeID, AsteroidID),
-	-- Also key the table by time and direction for searching later (?)
+	PRIMARY KEY (AsteroidID, Segment),
+	-- Also key the table by SkyPatchID and TimeID to facilitate searching for a SkyPatchID and TimeID to match a detection
+	INDEX IDX_SkyPatchID_TimeID (SkyPatchID, TimeID_0),
 	-- Foreign keys
-	CONSTRAINT FK_AsteroidSkyPatch_IntegrationTimeID FOREIGN KEY (IntegrationTimeID) REFERENCES KS.IntegrationTime(IntegrationTimeID),
 	CONSTRAINT FK_AsteroidSkyPatch_AsteroidID FOREIGN KEY (AsteroidID) REFERENCES KS.Asteroid(AsteroidID),
-	CONSTRAINT FK_AsteroidSkyPatch_SkyPatchID FOREIGN KEY (SkyPAtchID) REFERENCES KS.SkyPatch(SkyPatchID)
+	CONSTRAINT FK_AsteroidSkyPatch_SkyPatchID FOREIGN KEY (SkyPatchID) REFERENCES KS.SkyPatch(SkyPatchID),
+	CONSTRAINT FK_AsteroidSkyPatch_TimeID_0 FOREIGN KEY (TimeID_0) REFERENCES KS.HiResTime(TimeID),
+	CONSTRAINT FK_AsteroidSkyPatch_TimeID_1 FOREIGN KEY (TimeID_1) REFERENCES KS.HiResTime(TimeID)	
 )
 ENGINE='Aria' TRANSACTIONAL=0
 COMMENT = "For all known asteroids, at every IntegrationTimeSlice, compute the direction of the asteroid and its SkyPatch.";
