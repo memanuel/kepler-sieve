3bf1866d934721f77dd96912976d0d9d5e7b4b1d
Michael S. Emanuel <michael.s.emanuel@gmail.com>
2021-06-16 18:33:14 -0400

    Regeneration of KS.Detection in progress.

--- sql/02-create-table/082-Detection.sql
+++ sql/02-create-table/082-Detection.sql
@@ -1,15 +1,16 @@
 CREATE OR REPLACE TABLE KS.Detection(
-    -- Primary key and foreign keys
+    -- Primary key
+    DetectionID INT NOT NULL
+    	COMMENT "Primary key; also foreign key to the RawDetection table",
+    -- Foreign keys
     DetectionTimeID INT NOT NULL
         COMMENT "The time of this detection as a foreign key to the DetectionTime table",
+    -- Spatio-temporal index on (SkyPatchID, TimeID)
     SkyPatchID INT NOT NULL
     	COMMENT "Foreign key to SkyPatch table",
-    k TINYINT UNSIGNED NOT NULL
-    	COMMENT "Counter over all the detections sharing this sky patch",
-    -- Foreign key fields
-    DetectionID INT NOT NULL
-    	COMMENT "Foreign key to the RawDetection table",
-    -- Data payload
+    TimeID INT NOT NULL
+    	COMMENT "Integer ID for the time in minutes; foreign key to HiResTime table",
+	-- Data payload
     mjd DOUBLE NOT NULL
         COMMENT "The Modified Julian Date of this detection in the TDB (barycentric dynamical time) frame",
 	-- Unit direction u = [ux, uy, uz] in mean ecliptic frame
@@ -23,16 +24,18 @@ CREATE OR REPLACE TABLE KS.Detection(
     mag DOUBLE NOT NULL
     	COMMENT "Apparent magnitude (brightness) of this detection",
     -- Primary key
-    PRIMARY KEY (DetectionTimeID, SkyPatchID, k)
-    	COMMENT "Primary key optimized for spatial searching",
-    -- Unique key on DetectionID
-    UNIQUE KEY UNQ_Detection_DetectionID(DetectionID)
-    	COMMENT "Support joins based on DetectionID",
+    PRIMARY KEY (DetectionID)
+    	COMMENT "Primary key optimized for matching detections in a time range, then a SkyPatch",
+    INDEX IDX_Detection_SkyPatchID_DetectionTimeID (SkyPatchID, TimeID) 
+    	COMMENT "Support matching detections by location and time; SkyPatchID is first b/c it has higher cardinality",
 	-- Foreign keys
-    CONSTRAINT FK_Detection_DetectionTimeID
-        FOREIGN KEY (DetectionTimeID) REFERENCES KS.DetectionTime(DetectionTimeID),
     CONSTRAINT FK_Detection_DetectionID
         FOREIGN KEY (DetectionID) REFERENCES KS.RawDetection(DetectionID)
+    -- Skip FKs to avoid bloating the table
+--    CONSTRAINT FK_Detection_DetectionTimeID
+--        FOREIGN KEY (DetectionTimeID) REFERENCES KS.DetectionTime(DetectionTimeID)
+--    CONSTRAINT FK_Detection_TimeID
+--        FOREIGN KEY (TimeID) REFERENCES KS.HiResTime(TimeID)
 )
 ENGINE='Aria' TRANSACTIONAL=0
 COMMENT "Detections of possible asteroids across multiple data sources; enriched with unit direction and SkyPatch.";
--- sql/04-stored-procedure-get/044-GetRawDetections.sql
+++ sql/04-stored-procedure-get/044-GetRawDetections.sql
@@ -3,32 +3,33 @@ DELIMITER $$
 -- ********************************************************************************
 CREATE OR REPLACE 
 DEFINER = kepler
-PROCEDURE KS.GetRawDetections(
-    IN mjd0 INT,
-    IN mjd1 INT
-)
-COMMENT "Get raw detections in the specified date range.  Only return detections that have not been rolled up into KS.Detections."
+PROCEDURE KS.GetRawDetectionIDs()
+COMMENT "Get ID range of raw detection that have not yet been rolled up."
 
 BEGIN 
 
 SELECT
-	rd.DetectionTimeID,
-	rd.DetectionID,
-	dt.mjd,
-	rd.ra,
-	rd.`dec`,
-	rd.mag
+	MIN(rd.DetectionID) AS DetectionID_0,
+	MAX(rd.DetectionID) AS DetectionID_1
 FROM
-	KS.RawDetection AS rd
-	INNER JOIN KS.DetectionTime AS dt ON dt.DetectionTimeID = rd.DetectionTimeID
-WHERE
-	mjd0 <= dt.mjd AND dt.mjd < mjd1
-    -- Only detections that have not already been rolled up into KS.Detection
-    AND NOT EXISTS (
-        SELECT d.DetectionID
-        FROM KS.Detection AS d
-        WHERE d.DetectionID = rd.DetectionID
-    );
+	KS.RawDetection AS rd;
+
+END
+$$
+
+-- ********************************************************************************
+CREATE OR REPLACE 
+DEFINER = kepler
+PROCEDURE KS.GetDetectionIDs()
+COMMENT "Get ID range of detection that have been rolled up."
+
+BEGIN 
+
+SELECT
+	MIN(det.DetectionID) AS DetectionID_0,
+	MAX(det.DetectionID) AS DetectionID_1
+FROM
+	KS.Detection AS det;
 
 END
 $$
@@ -38,6 +39,8 @@ CREATE OR REPLACE
 DEFINER = kepler
 PROCEDURE KS.GetRawDetectionDates()
 COMMENT "Get date range of raw detection that have not yet been rolled up."
+
+BEGIN 
 
 SELECT
 	CAST(FLOOR(MIN(dt.mjd)) AS INT) AS mjd0,
@@ -49,6 +52,44 @@ WHERE NOT EXISTS (
 	FROM KS.Detection AS d
 	WHERE d.DetectionTimeID = dt.DetectionTimeID
     );
+
+END
+$$
+
+-- ********************************************************************************
+CREATE OR REPLACE 
+DEFINER = kepler
+PROCEDURE KS.GetRawDetections(
+    IN DetectionID_0 INT,
+    IN DetectionID_1 INT
+)
+COMMENT "Get raw detections in the specified DetectionID range.  Only return detections that have not been rolled up into KS.Detections."
+
+BEGIN 
+
+SELECT
+	rd.DetectionID,
+	rd.DetectionTimeID,
+	dt.mjd,
+	rd.ra,
+	rd.`dec`,
+	rd.mag
+FROM
+	-- Start with RawDetection table
+	KS.RawDetection AS rd
+	-- Join DetectionTime table to get mjd of this detection
+	INNER JOIN KS.DetectionTime AS dt ON dt.DetectionTimeID = rd.DetectionTimeID
+WHERE
+	-- Only selected range of DetectionIDs
+	DetectionID_0 <= rd.DetectionID AND rd.DetectionID < DetectionID_1
+    -- Only detections that have not already been rolled up into KS.Detection
+    AND NOT EXISTS (
+        SELECT d.DetectionID
+        FROM KS.Detection AS d
+        WHERE d.DetectionID = rd.DetectionID
+    );
+END
+$$
 
 -- ********************************************************************************
 DELIMITER ;
