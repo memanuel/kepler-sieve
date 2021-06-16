1d45d5821a5adc9f36ee2eb4419ce1f93d70f941
Michael S. Emanuel <michael.s.emanuel@gmail.com>
2021-06-16 14:09:58 -0400

    regenerating DB table KS.Detection with detection_enrich.py

--- src/detection_enrich.py
+++ src/detection_enrich.py
@@ -11,16 +11,14 @@ Michael S. Emanuel
 import numpy as np
 import pandas as pd
 
-# Astronomy related
-from astropy.units import deg
-
 # UI
 from tqdm.auto import tqdm as tqdm_auto
 
 # MSE imports
 from db_utils import df2db, sp2df, sp_run
 from ra_dec import radec2dir
-from sky_patch import dir2SkyPatchID
+from orbital_element import unpack_vector
+from sky_patch import dir2SkyPatchID, N_sp
 
 # ********************************************************************************************************************* 
 def detection_add_dir(df: pd.DataFrame):
@@ -31,31 +29,32 @@ def detection_add_dir(df: pd.DataFrame):
     OUTPUTS:
         Modifies df in place.
     """
-    # Extract mjd, ra, and dec as vectors of astropy angles
+    # Extract mjd, ra, and dec as numpy arrays; ra and dec flat arrays in degrees
     mjd = df.mjd.values
-    ra = df.ra.values * deg
-    dec = df.dec.values * deg
+    ra = df.ra.values
+    dec = df.dec.values
 
-    # Compute the directions using radec2dir()
-    u = radec2dir(ra=ra, dec=dec, obstime_mjd=mjd)    
+    # Compute the directions using radec2dir() and unpack it
+    u = radec2dir(ra=ra, dec=dec, mjd=mjd)
+    ux, uy, uz = unpack_vector(u)
 
     # Add these directions to the DataFrame in three columns after dec
     col_num_dec = df.columns.get_loc('dec')
-    df.insert(loc=col_num_dec+1, column='ux', value=u[0])
-    df.insert(loc=col_num_dec+2, column='uy', value=u[1])
-    df.insert(loc=col_num_dec+3, column='uz', value=u[2])
+    df.insert(loc=col_num_dec+1, column='ux', value=ux)
+    df.insert(loc=col_num_dec+2, column='uy', value=uy)
+    df.insert(loc=col_num_dec+3, column='uz', value=uz)
 
 # ********************************************************************************************************************* 
-def calc_detections(mjd0: int, mjd1: int, N_sky_patch: int):
+def calc_detections(mjd0: int, mjd1: int, N_sp: int):
     """
     Assemble a batch of detections for insertion to Database from RawDetection data.
     INPUTS:
         mjd0: First date of detections (inclusive)
         mjd1: Last date of detections (exclusive)
-        N_sky_patch: Grid size used for SkyPatch
+        N_sp: Grid size used for SkyPatch
     RETURNS:
         df:   DataFrame ready to insert.
-              Columns before enrichment: DetectionTimeID, DetectionID, mjd, ra, dec, mag
+              Columns before enrichment: DetectionID, DetectionTimeID, mjd, ra, dec, mag
               Columns added: ux, uy, uz, SkyPatchID, k
     """
 
@@ -67,21 +66,17 @@ def calc_detections(mjd0: int, mjd1: int, N_sky_patch: int):
     if df.shape[0]==0:
         return pd.DataFrame(columns = list(df.columns) + ['ux', 'uy', 'uz', 'SkyPatchID', 'k'])
 
-    # Caclulated HiResTimeID field and add to DataFrame
-    time_id = np.round(df.mjd.values * 1440.0).astype(np.int)
-    df['HiResTimeID'] = time_id
-
     # Calculate direction and add it to DataFrame
     detection_add_dir(df)
     # Nx3 array of directions
     dir = df[['ux', 'uy', 'uz']].values
 
     # Calculate and add the SkyPatchID
-    SkyPatchID = dir2SkyPatchID(dir=dir, N=N_sky_patch)
+    SkyPatchID = dir2SkyPatchID(dir=dir, N=N_sp)
     df['SkyPatchID'] = SkyPatchID
 
     # Add the field k for the detection number in each SkyPatch
-    k = df.groupby(['DetectionTimeID', 'SkyPatchID']).cumcount() +1
+    k = df.groupby(['SkyPatchID', 'DetectionTimeID']).cumcount() +1
     df['k'] = k
 
     return df
@@ -91,12 +86,8 @@ def write_detections():
     """
     Write to KS.Detection table from KS.RawDetection
     """
-
-    # Set grid size for SkyPatch
-    N_sky_patch: int = 1024
-
     # Columns to insert to DB
-    columns = ['HiResTimeID', 'DetectionTimeID', 'SkyPatchID', 'k', 'DetectionID', 'mjd', 'ux', 'uy', 'uz', 'mag']
+    columns = ['DetectionID', 'SkyPatchID', 'DetectionTimeID', 'k', 'mjd', 'ux', 'uy', 'uz', 'mag']
 
     # Get date range to roll up
     dts = sp2df('KS.GetRawDetectionDates')    
@@ -113,7 +104,7 @@ def write_detections():
         mjd0 = mjd0_all + i * sz
         mjd1 = mjd0 + sz
         # Calculate the detections
-        df = calc_detections(mjd0=mjd0, mjd1=mjd1, N_sky_patch=N_sky_patch)
+        df = calc_detections(mjd0=mjd0, mjd1=mjd1, N_sp=N_sp)
         # Insert into DB table
         df2db(df=df, schema='KS', table='Detection', columns=columns, progbar=False)
         
@@ -152,14 +143,14 @@ def main():
     """
 
     # Write from KS.RawDetection to KS.Detection
-    # write_detections()
+    write_detections()
 
-    # Get last DetectionPairID already loaded onto KS.Tracklet
-    ldtp = sp2df('KS.GetLastTrackletTimePair').LastDetectionTimePairID[0]
-    print(f'Last DetectionTimePairID loaded into KS.Tracklet is {ldtp}.')
+    # # Get last DetectionPairID already loaded onto KS.Tracklet
+    # ldtp = sp2df('KS.GetLastTrackletTimePair').LastDetectionTimePairID[0]
+    # print(f'Last DetectionTimePairID loaded into KS.Tracklet is {ldtp}.')
 
-    # Write from KS.Detection to KS.Tracklet
-    write_tracklets(sz=500, start=ldtp)
+    # # Write from KS.Detection to KS.Tracklet
+    # write_tracklets(sz=500, start=ldtp)
 
 # ********************************************************************************************************************* 
 if __name__ == '__main__':
