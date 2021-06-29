6025ee7430a02dde95d7cb695a51c75033334f80
Michael S. Emanuel <michael.s.emanuel@gmail.com>
2021-06-29 18:44:34 -0400

    working on lightweight version of detection table with only required fields to save memory use

--- src/Detection.cpp
+++ src/Detection.cpp
@@ -13,13 +13,15 @@
 // Local names used
 using ks::Detection;
 using ks::DetectionTable;
+using ks::DetectionCandidate;
+using ks::DetectionCandidateTable;
 
 // Set batch size to one million
 constexpr int batch_size = 1000000;
 
 // *****************************************************************************
-/** Helper function: Process a batch of rows, 
- * writing data from stored preocedure output to vector of detections. */
+/**Helper function: Process a batch of rows, 
+ * writing data from stored procedure output to vector of detections. */
 void process_rows(db_conn_type& conn, vector<Detection>& dt, vector<vector<int32_t>>& dtsp, int d0, int d1)
 {
     // Run the stored procedure to get detections including the observatory position
@@ -141,6 +143,120 @@ Detection DetectionTable::operator[](int32_t id) const
 
 // *****************************************************************************
 vector<int32_t> DetectionTable::get_skypatch(int32_t spid) const
+{
+    return dtsp[spid];
+} // function
+
+// *****************************************************************************
+// Analogous functions for lightweigh DetectionCandidate version
+// *****************************************************************************
+
+// *****************************************************************************
+/**Helper function: Process a batch of rows, 
+ * writing data from stored procedure output to vector of detections. */
+void process_rows(db_conn_type& conn, vector<DetectionCandidate>& dt, vector<vector<int32_t>>& dtsp, int d0, int d1)
+{
+    // Run the stored procedure to get detections; does not need the observatory positions
+    string sp_name = "KS.GetDetectionCandidates";
+    vector<string> params = {to_string(d0), to_string(d1)};
+    ResultSet *rs = sp_run(conn, sp_name, params);
+
+    // Loop through resultset
+    while (rs->next()) 
+    {
+        // Unpack the fields in the resultset; 10 total fields
+        // 3 key fields
+        int32_t detection_id = rs->getInt("DetectionID");
+        int32_t sky_patch_id = rs->getInt("SkyPatchID");
+        int32_t time_id = rs->getInt("TimeID");
+        // Initialize the DetectionCandidate at this location in dt
+        int idx = detection_id - d0;
+        dt[idx] = {
+            .detection_id = detection_id,
+            .sky_patch_id = sky_patch_id,
+            .time_id = time_id,
+        };
+
+        // Write this DetectionID to the vector keyed by this SkyPatchID
+        (dtsp[sky_patch_id]).push_back(detection_id);
+    }   // while rs
+    // Close the resultset
+    rs->close();
+}
+
+// *****************************************************************************
+DetectionCandidateTable::DetectionCandidateTable(db_conn_type &conn, int d0, int d1, bool progbar): 
+    d0(d0),
+    d1(d1),
+    // Initialize dt to a vector with sz entries, one for each possible detection in the interval
+    dt(vector<DetectionCandidate>(d1-d0)),
+    // Initialize dtsp to a vector with N_sp entries, one for each SkyPatch (whether occupied or not)
+    dtsp(vector<vector<int32_t>>(N_sp))
+{
+    // Write 0 into DetectionID field so we can later ignore any holes (e.g. DetectionID=0)
+    int sz = d1-d0;
+    for (int i=0; i<sz; i++)
+    {
+        dt[i].detection_id=0;
+    }
+
+    // Calculate number of batches
+    int batch_count = std::max((sz+1)/batch_size, 1);
+    // Status update
+    if (progbar) 
+    {
+        print("Processing {:d} detection candidates from {:d} to {:d} in {:d} batches of {:d}...\n",
+                sz, d0, d1, batch_count, batch_size);
+    }
+
+    // Timer for processing detections from DB
+    Timer t;
+    t.tick();
+
+    // Iterate over the batches
+    for (int i0=d0; i0<d1; i0+=batch_size)
+    {
+        // Upper limit for this batch
+        int i1 = std::min(i0+batch_size, d1);
+        // Process SQL data in this batch
+        process_rows(conn, dt, dtsp, i0, i1);
+        // Progress bar
+        if (progbar) 
+        {
+            print("."); 
+            flush_console();
+        }
+    }
+    if (progbar) 
+    {
+        print("\nLoaded detection candidates table.\n");
+        t.tock_msg();
+    }
+}
+
+// *****************************************************************************
+DetectionCandidateTable::DetectionCandidateTable(db_conn_type &conn, bool progbar): 
+    // Delegate to range constructor, using DB to compute d1
+    DetectionCandidateTable(conn, 0, sp_run_int(conn, "KS.GetMaxDetectionID"), progbar) {}
+
+// *****************************************************************************
+///Default destructor is OK here
+DetectionCandidateTable::~DetectionCandidateTable() {}
+
+// *****************************************************************************
+const int DetectionCandidateTable::size() const
+{
+    return (d1-d0);
+}
+
+// *****************************************************************************
+DetectionCandidate DetectionCandidateTable::operator[](int32_t id) const
+{
+    return dt[id];
+}
+
+// *****************************************************************************
+vector<int32_t> DetectionCandidateTable::get_skypatch(int32_t spid) const
 {
     return dtsp[spid];
 } // function
--- src/Detection.hpp
+++ src/Detection.hpp
@@ -60,6 +60,19 @@ struct Detection
 };
 
 // *****************************************************************************
+/**Subset of data contained in one detection needed to search for candidate detections near an asteroid.
+ * See DB table KS.Detection and stored procedure KS.GetDetectionObs.*/
+struct DetectionCandidate
+{
+    /// Integer ID of this detection
+    int32_t detection_id;
+    /// Integer ID of the SkyPatch where this detection is located in the sky
+    int32_t sky_patch_id;
+    /// Integer ID of the detection time; FK to KS.HiResTime; the mjd converted to number of minutes.
+    int32_t time_id;
+};
+
+// *****************************************************************************
 class DetectionTable
 {
 public:
@@ -86,6 +99,37 @@ public:
 private:
     /// Vector of detections; dt stands for "Detection Table"
     vector<Detection> dt;
+    /// Vector of detection ID vectors keyed by SkyPatchID; dtsp stands for "Detection Table [keyed by] SkyPatchID"
+    vector< vector<int32_t> > dtsp;
+};
+
+// *****************************************************************************
+class DetectionCandidateTable
+{
+public:
+    /// Initialize a DetectionCandidateTable object with detections in the given range
+    DetectionCandidateTable(db_conn_type &conn, int d0, int d1, bool progbar);
+    /// Initialize a DetectionCandidateTable object with all available detections
+    DetectionCandidateTable(db_conn_type &conn, bool progbar);
+    /// Destructor for DetectionCandidateTable.
+    ~DetectionCandidateTable();
+
+    /// Get a detection candidate given its ID
+    DetectionCandidate operator[](int32_t id) const;
+
+    /// Get vector of DetectionIDs matching a given SkyPatchID
+    vector<int32_t> get_skypatch(int32_t spid) const;
+
+    /// First detection ID loaded; base for indexing into arrays
+    const int d0;
+    /// Last detection ID loaded
+    const int d1;
+    /// The size
+    const int size() const;
+
+private:
+    /// Vector of detections; dt stands for "Detection Table"
+    vector<DetectionCandidate> dt;
     /// Vector of detection ID vectors keyed by SkyPatchID; dtsp stands for "Detection Table [keyed by] SkyPatchID"
     vector< vector<int32_t> > dtsp;
 };
