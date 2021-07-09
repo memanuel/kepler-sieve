8c59d2c45e217c9d62f945655ae56e16fb08137c
Michael S. Emanuel <michael.s.emanuel@gmail.com>
2021-07-09 09:53:27 -0400

    reorganized project directories to move python source into src_py

--- jupyter/kepler_sieve.py
+++ jupyter/kepler_sieve.py
@@ -1,6 +1,6 @@
 import os
 import sys
 
-module_path = os.path.abspath(os.path.join('../src'))
+module_path = os.path.abspath(os.path.join('../src_py'))
 if module_path not in sys.path:
     sys.path.append(module_path)
